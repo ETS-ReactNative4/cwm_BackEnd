179afbb64cc25ef3fc50b6aac769aba8dcac4ac7
CALuthfi <choiriahmadluthfi@gmail.com>
2019-04-13 23:18:10 +0700

    Extraacting a Logger Service

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Logger from "./services/logService";
 import registerServiceWorker from "./registerServiceWorker";
-import Raven from "raven-js";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Raven.config("https://05323d37c9a947eba9daaaab1e6171a9@sentry.io/1249956", {
-  release: "1-0-0",
-  environment: "development-test"
-}).install();
+Logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
